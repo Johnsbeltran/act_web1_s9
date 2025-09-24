298435d2d92aba233d4eff874a897e7741cbdef1
Johnsbeltran <jbeltran@cesde.net>
2025-09-24 12:34:51 -0500

    Update ejercicio_02.js

---
+++ ejercicios/ejercicio_02.js
@@ -0,0 +1,14 @@
+const titleselector = document.getElementById('title1');
+const barselector = document.getElementById('mainbar');
+const listselector = document.getElementById('firstlist');
+
+
+titleselector.textContent = 'Cambio desde JavaScript';
+
+let listitems = listselector.getElementsByTagName('li');
+listItems[0].textContent = 'First modified element';
+listItems[1].textContent = 'Second modified element';
+listItems[2].textContent = 'Third modified element';
+
+//modify colors
+navElement.style.backgroundColor = '#a51717ff';
